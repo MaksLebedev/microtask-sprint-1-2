387a0988ec5ec2e9aa63268aab8195f1e474380a
Maksim Lebedev <monk527@yandex.ru>
2024-07-28 02:24:05 +0500

    add microtask about Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { FullInput } from "./components/FullInput";
+import { title } from "process";
+import { Input } from "./components/Input";
+import { Button } from "./components/Button";
 
 function App() {
+  const [message, setMessage] = useState([
+    { message: "message1" },
+    { message: "message2" },
+    { message: "message3" },
+  ]);
+
+  let [title, setTitle] = useState("");
+
+  // const addMessage = (title: string) => {
+  //   setMessage([{ message: title }, ...message]);
+  // };
+
+  const callbackButtonHandler = () => {
+    setMessage([{ message: title }, ...message]);
+    setTitle("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <FullInput addMessage={addMessage} /> */}
+      <Input setTitle={setTitle} title={title} />
+      <Button name={"+"} callback={callbackButtonHandler}  />
+      {message.map((el, index) => {
+        return <div key={index}>{el.message}</div>;
+      })}
     </div>
   );
 }
